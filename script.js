d58b1a5da21d43b51782e46dce5743a639dba4ff
nelsonjm <njoeden05@gmail.com>
2021-11-17 09:34:11 -0800

    done

---
+++ script.js
@@ -0,0 +1,27 @@
+console.log('my script is working')
+
+prompt('do you like video games')
+
+let answer = prompt('do you like first person shooters')
+
+if (answer === "yes"){
+    console.log("Call of Duty")
+    console.log("Apex Legends") 
+    console.log("Rust")
+    console.log("Battlefeild")
+    console.log("Splitgate")
+    console.log("Overwatch") 
+    console.log("Valarant") 
+    console.log("Halo")
+}
+
+if (answer === "no"){
+    console.log("minecraft")
+    console.log("Ark")
+    console.log("forza")
+    console.log("phasmopbia") 
+    console.log("skyrim") 
+    console.log("rocketleague") 
+    console.log("fifa") 
+    console.log("madden") 
+}
